03cfb61af6f01410c8adcf54abec9211b2f25303
Митрофанов Владимир Владимирович <v.mitrofanov@upc4.baikal.int>
2017-10-13 11:48:18 +0300

    add boards

---
+++ boards/BE_BT_BFK16/board.mk
@@ -0,0 +1,7 @@
+# List of all the board related files.
+#BOARDSRC = ${CHIBIOS}/os/hal/boards/BE_BT_BFK16/board.c
+BOARDSRC = boards/BE_BT_BFK16/board.c
+
+# Required include directories
+#BOARDINC = ${CHIBIOS}/os/hal/boards/BE_BT_BFK16
+BOARDINC = boards/BE_BT_BFK16
